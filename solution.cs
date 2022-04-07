e6e1f7551051425628cd324a02a08724727eb4e4
Kooreis <kooreis.official@gmail.com>
2022-04-07 14:36:32 -0500

    This is the main function that counts the number of islands in the grid. It iterates through each cell in the grid. If the cell is land ('1'), it performs a depth-first search (DFS) and increments the island count.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a JavaScript solution using Depth-First Search (DFS) algorithm:
-
-```javascript
 function numIslands(grid) {
     if (grid === null || grid.length === 0) {
         return 0;
@@ -18,30 +15,3 @@ function numIslands(grid) {
 
     return numberOfIslands;
 }
-
-function dfs(grid, i, j) {
-    if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') {
-        return 0;
-    }
-
-    grid[i][j] = '0';
-
-    dfs(grid, i - 1, j);
-    dfs(grid, i + 1, j);
-    dfs(grid, i, j - 1);
-    dfs(grid, i, j + 1);
-
-    return 1;
-}
-
-let grid = [
-    ['1', '1', '1', '1', '0'],
-    ['1', '1', '0', '1', '0'],
-    ['1', '1', '0', '0', '0'],
-    ['0', '0', '0', '0', '0']
-];
-
-console.log(numIslands(grid));
-```
-
-This console application will output the number of islands in the 2D grid map. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
