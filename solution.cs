d3479050cb1b32f87fb9c26ec2985dc0cb30ad47
Kooreis <kooreis.official@gmail.com>
2022-04-08 14:36:32 -0500

    This is the DFS function. It checks if the current cell is out of bounds or is water ('0'). If so, it returns 0. Otherwise, it marks the current cell as visited (by setting it to '0') and performs DFS on its four neighbors.

--- solution.cs
+++ solution.cs
@@ -1,17 +1,14 @@
-function numIslands(grid) {
-    if (grid === null || grid.length === 0) {
+function dfs(grid, i, j) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') {
         return 0;
     }
 
-    let numberOfIslands = 0;
+    grid[i][j] = '0';
 
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if (grid[i][j] === '1') {
-                numberOfIslands += dfs(grid, i, j);
-            }
-        }
-    }
+    dfs(grid, i - 1, j);
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j - 1);
+    dfs(grid, i, j + 1);
 
-    return numberOfIslands;
+    return 1;
 }
