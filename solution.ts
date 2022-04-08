8bdc4b342da9b20c5dc4aaa491c58f0b54f0cb41
Kooreis <kooreis.official@gmail.com>
2022-04-08 14:36:32 -0500

    The numIslands method is defined. It initializes the grid and visited cells, then iterates over the grid. If a cell is land ('1') and has not been visited, it starts a DFS from that cell and increments the island count.

--- solution.ts
+++ solution.ts
@@ -1,6 +1,20 @@
-class Solution {
-    private grid: string[][];
-    private visited: boolean[][];
-    private dx: number[] = [-1, 0, 1, 0];
-    private dy: number[] = [0, 1, 0, -1];
-}
+public numIslands(grid: string[][]): number {
+        if (grid === null || grid.length === 0) {
+            return 0;
+        }
+
+        this.grid = grid;
+        this.visited = Array.from({ length: grid.length }, () => Array(grid[0].length).fill(false));
+
+        let islands = 0;
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] === '1' && !this.visited[i][j]) {
+                    this.dfs(i, j);
+                    islands++;
+                }
+            }
+        }
+
+        return islands;
+    }
